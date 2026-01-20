f82e87869d9b580fc8c9d8255ccd63b10876b92c
tony22-cyber <umaiea262@gmail.com>
2026-01-20 15:56:46 +0800

    Create RadioBtn.vue

---
+++ src/components/RadioBtn.vue
@@ -0,0 +1,10 @@
+<template>
+    <span style="color: green">{{props.node?.content?? "无数据"}}</span>
+</template>
+<script setup>
+    const props = defineProps({ 
+        node: { type: Object, required: false }
+    })
+     // 无法获取节点内容且raw的结束标识错误，如：（raw:<RadioBtn></radiobtn>）
+    debugger;
+</script>
