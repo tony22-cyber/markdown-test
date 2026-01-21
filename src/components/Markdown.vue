e769b19b94ee91ddd6f6ef470d84135ea3fb6dd4
tony22-cyber <umaiea262@gmail.com>
2026-01-21 15:03:35 +0800

    Update Markdown.vue

--- src/components/Markdown.vue
+++ src/components/Markdown.vue
@@ -19,29 +19,45 @@
 
     const CustomHtmlTags = ["TaskPlan", "AnalysisSteps", "RadioBtn"];
 
-    const content =  `
-<hide>uid:6421ed2c189341e08eae49375bc1032f</hide>
-
+// 错误示例
+const content =  `
 <TaskPlan>
 已收到您关于刻画xx组织机构画像的任务请求，我将按照以下步骤执行该任务：                            
 - 匹配并确认目标组织机构
 - 构建组织机构画像管理数据
 </TaskPlan>
-
-<AnalysisSteps>
-# 匹配信息和确认组织机构
-## 深度思考
-- 好的，我现在需要处理用户的输入“刻画xx组织机构画像”，根据规则提取组织机构名称。首先，用户提到的是“xx组织机构”..
-## 组织机构查询关键字:XX
-</AnalysisSteps>
-
+测试案例
 <RadioBtn>
 <title>组织结构列表</title>
 <content>少时诵诗书</content>
 </RadioBtn>
+`
+// 正常示例
+const case1 =  `
+<TaskPlan>
+已收到您关于刻画xx组织机构画像的任务请求，我将按照以下步骤执行该任务：                            
+- 匹配并确认目标组织机构
+- 构建组织机构画像管理数据
+</TaskPlan>
+测试案例
+<RadioBtn>
+    <title>组织结构列表</title>
+    <content>少时诵诗书</content>
+</RadioBtn>
+`
+// 正常示例
+const case2 =  `
+<TaskPlan>
+已收到您关于刻画xx组织机构画像的任务请求，我将按照以下步骤执行该任务：                            
+- 匹配并确认目标组织机构
+- 构建组织机构画像管理数据
+</TaskPlan>
+<RadioBtn>
+<title>组织结构列表</title>
+<content>少时诵诗书</content>
+</RadioBtn>
+`
 
-<hide>库中没有该目标相关数据，无法生成机构画像。</hide>
-    `
     setCustomComponents(customId, {
         taskplan: AnalysisPlan,
         analysissteps: AnalysisSteps,
